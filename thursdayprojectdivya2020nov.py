c5aac66af05b52a3d850e5da77a30806becd403b
Divya Patil <74003649+divya-mjfan1958@users.noreply.github.com>
2020-11-05 18:23:29 +0400

    Add files via upload

---
+++ thursdayprojectdivya2020nov.py
@@ -0,0 +1,158 @@
+print("Welcome to Quizlet!")
+print("Today you will have to answer 10 random questions! Hope you are ready!")
+print("you will get 10 marks for each correct answer. \n there is no negative marking")
+print("Lets start the quiz!")
+print(" Note: please type all the answers in capital letters eg : A, B etc")
+score = 0
+print("score = 0")
+print("Q1 Which is the most populated state in India?")
+print("a) Bihar b) Maharashtra \n c) Uttar Pradesh  d) Madhya Pradesh")
+answer1 = input()
+if answer1 == "A":
+    print("wrong answer! \n the right answer is c) Uttar Pradesh")
+elif answer1 == "B":
+    print("wrong answer! \n the right answer is c) Uttar Pradesh")
+elif answer1 == "C":
+    print("correct answer!")
+    score = score + 10
+elif answer1 == "D":
+    print("wrong answer! \n the right answer is c) Uttar Pradesh")
+
+print(" ")
+print("Q2 Who is Donald Trump's rival in this years presidential elections?")
+print("(a) Kamala Harris  b) Joe Biden \n c) Hillary Clinton  d) Rudy Giuliani")
+answer2 = input()
+if answer2 == "A":
+    print("wrong answer! \n the right answer is b) Joe Biden")
+elif answer2 == "B":
+    print("correct answer!")
+    score = score + 10
+elif answer2 == "C":
+    print("wrong answer! \n the right answer is b) Joe Biden")
+elif answer2 == "D":
+    print("wrong answer! \n the right answer is b) Joe Biden")
+
+print(" ")
+print("Q3 Who made the world's biggest selling music album?")
+print("a) Michael Jackson  b)Miley Cyrus \n c) Post Malone  d) Shakira")
+answer3 = input()
+if answer3 == "A":
+    print("correct answer!")
+    score = score + 10
+elif answer3 == "B":
+    print("wrong answer!\nthe right answer is a) Michael Jackson. His album 'Thriller' is the most sold album in the world")
+elif answer3 == "C":
+    print("wrong answer!\nthe right answer is a) Michael Jackson. His album 'Thriller' is the most sold album in the world")
+elif answer3 == "D":
+    print("wrong answer!\nthe right answer is a) Michael Jackson. His album 'Thriller' is the most sold album in the world")
+
+print(" ")
+print("Q4 Which of the monarchs was the the ruler of China in 386-557 BCE?")
+print("a) Queen Elizabeth II  b) Louis XVI \n c) Xiongnu d) Xi Jinping")
+answer4 = input()
+if answer4 == "A":
+    print("wrong answer! \n the right answer is c) Xiongnu")
+elif answer4 == "B":
+    print("wrong answer! \n the right answer is c) Xiongnu")
+elif answer4 == "C":
+    print("correct answer!")
+    score = score + 10
+elif answer4 == "D":
+    print("wrong answer! \n the right answer is c) Xiongnu")
+
+print(" ")
+print("Q5 What is the presidential term in USA?")
+print("a) 5 years b) 4 years \n c) 6 years  d) 3 years")
+answer5 = input()
+if answer5 == "A":
+    print("wrong answer! \n the right answer is b) 4 years")
+elif answer5 == "B":
+    print("correct answer!")
+    score = score + 10
+elif answer5 == "C":
+    print("wrong answer! \n the right answer is b) 4 years")
+elif answer5 == "D":
+    print("wrong answer! \n the right answer is b) 4 years")
+
+print(" ")
+print("Q6 Who wrote the play 'Hamlet' ?")
+print("a) William Wordsworth  b) Ruskin Bond \n c) Robert Frost d) William Shakespeare")
+answer6 = input()
+if answer6 == "A":
+    print("wrong answer! \n the right answer is d) William Shakespeare")
+elif answer6 == "B":
+    print("wrong answer! \n the right answer is d) William Shakespeare")
+elif answer6 == "C":
+    print("wrong answer! \n the right answer is d) William Shakespeare")
+elif answer6 == "D":
+    print("correct answer!")
+    score = score + 10
+
+print(" ")
+print("Q7 Who made the dance step 'Moonwalk' famous?")
+print("a) James Brown b) Elvis Presley \n c) Michael Jackson d) Usher e) Chris Tucker")
+answer7 = input()
+if answer7 == "A":
+    print("wrong answer! \n the right answer is c) Michael Jackson")
+elif answer7 == "B":
+    print("wrong answer! \n the right answer is c) Michael Jackson")
+elif answer7 == "C":
+    print("correct answer!")
+    score = score + 10
+elif answer7 == "D":
+    print("wrong answer! \n the right answer is c) Michael Jackson")
+elif answer7 == "E":
+    print("wrong answer! \n the right answer is c) Michael Jackson")
+
+print(" ")
+print("Q8 What are the colors in the Indian National Flag?")
+print(
+    "a) Red, white, blue  b) Orange, white, green \n c) Yellow, black, read  d) Black, Green, red, white  e) Orange, white, green, blue(wheel with spikes)")
+answer8 = input()
+if answer8 == "A":
+    print("wrong answer! \n the right answer is e) Orange, white, green, blue(wheel with spikes")
+elif answer8 == "B":
+    print("wrong answer! \n the right answer is e) Orange, white, green, blue(wheel with spikes")
+elif answer8 == "C":
+    print("wrong answer! \n the right answer is e) Orange, white, green, blue(wheel with spikes")
+elif answer8 == "D":
+    print("wrong answer! \n the right answer is e) Orange, white, green, blue(wheel with spikes")
+elif answer8 == "E":
+    print("correct answer!")
+    score = score + 10
+
+print(" ")
+print("Q9 What is the capital of Pakistan?")
+print("a) Umarkot  b) Islamabad \n c) Lahore  d)Karachi  e)Faisalabad ")
+answer9 = input()
+if answer9 == "A":
+    print("wrong answer! \n the right answer is b) Islamabad")
+elif answer9 == "B":
+    print("correct answer!")
+    score = score + 10
+elif answer9 == "C":
+    print("wrong answer! \n the right answer is b) Islamabad")
+elif answer9 == "D":
+    print("wrong answer! \n the right answer is b) Islamabad")
+elif answer9 == "E":
+    print("wrong answer! \n the right answer is b) Islamabad")
+
+print(" ")
+print("Q10 Who was the founder of the Mughal rule in India?")
+print("a) Babur(Babar)  b) Jalaluddin Akbar \n c) Humayun  d) Aurangzeb e) Shah Jahan")
+answer10 = input()
+if answer10 == "A":
+    print("correct answer!")
+    score = score + 10
+elif answer10 == "B":
+    print("wrong answer! \n the right answer is a) Babur(Babar)")
+elif answer10 == "C":
+    print("wrong answer! \n the right answer is a) Babur(Babar)")
+elif answer10 == "D":
+    print("wrong answer! \n the right answer is a) Babur(Babar)")
+elif answer10 == "E":
+    print("wrong answer! \n the right answer is a) Babur(Babar)")
+
+print("\n \n \n'THERE IS A DIFFERENCE BETWEEN NOT KNOWING AND NOT KNOWING YET'")
+print("I Hope you enjoyed playing my quiz!")
+print("Your Score:", score)
